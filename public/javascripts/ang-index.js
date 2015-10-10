38962d072f012d89c857d06ad85ea2b85087ced6
Krishna Kalubandi <krishnakalubandi@gmail.com>
2015-10-10 17:28:45 +0530

    Functional app completed.

--- public/javascripts/ang-index.js
+++ public/javascripts/ang-index.js
@@ -1,4 +1,23 @@
-var app = angular.module('pulse', ['ngMaterial']);
-app.controller('home',function($scope){
+var app = angular.module('pulse', ['ngMaterial' , 'ngToast']);
+app.controller('home',function($scope,ngToast, $http){
 
+
+    $scope.submit = function(){
+
+        if($scope.qstring == null){
+            ngToast.create('song name is required!');
+        }
+        else {
+            var getreq = 'http://localhost:3000/search?qstring=' + ($scope.qstring.split(' ').join('%20'));
+            $http.get(getreq).success(function(response){
+                if(response.reply == "ok") {
+
+                    $scope.songs = response.data;
+                }
+                else {
+                    ngToast.create('Sorry song doesnt exist in the database');
+                }
+            });
+        }
+    }
 });
